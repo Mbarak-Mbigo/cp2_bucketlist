cb76d0966170dfd2b5b4215b9bfd0e5fa805055d
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-04-15 12:24:04 +0300

    [ch-create-models] create models

--- api/models.py
+++ api/models.py
@@ -1,11 +1,49 @@
+"""API version 1.0 models."""
+from datetime import datetime
+from v1 import db
 
 
 class User(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    is_admin = db.Column(db.Boolean, default=True)
+    created_date = db.Column(db.DateTime, nullable=False,
+                             default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 class BucketList(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.Text)
+    status = db.Column(db.String(30), default='created')
+    created_date = db.Column(db.DateTime, nullable=False,
+                             default=datetime.utcnow)
+    accomplished_date = db.Column(db.DateTime)
+    closed_date = db.Column(db.DateTime)
+    open_duration = db.Column(db.Integer)
+    items = db.relationship('BucketItem', backref='bucketlist', lazy=True)
+
+    def __repr__(self):
+        return '<BucketList %r>' % self.name
 
 
 class BucketItem(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.Text)
+    created_date = db.Column(db.DateTime, nullable=False,
+                             default=datetime.utcnow)
+    accomplished_date = db.Column(db.DateTime)
+    closed_date = db.Column(db.DateTime)
+    open_duration = db.Column(db.Integer)
+    bucket_id = db.Column(db.Integer, db.ForeignKey('bucket.id'),
+                          nullable=False)
+
+    def __repr__(self):
+        return '<BucketItem %r>' % self.name
